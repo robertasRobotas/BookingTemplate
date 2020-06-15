8d635c93f73a584d1048c1a91420fd91975765eb
Robertas <r.ankudovicius@gmail.com>
2020-06-16 00:12:30 +0300

    added info to addInfo screen

---
+++ front/src/components/pages/UserInformationForm/UserInformationForm.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Wrapper from '../../_hoc/Wrapper';
+import { observer } from 'mobx-react';
+import './UserInformationForm.css';
+
+const UserInformationForm = ({ history, location, rootModel }) => {
+  return (
+    <>
+      <form>
+        <label for='fname'>First Name</label>
+        <input
+          type='text'
+          id='fname'
+          name='firstname'
+          placeholder='Your name..'
+        />
+
+        <label for='lname'>Last Name</label>
+        <input
+          type='text'
+          id='lname'
+          name='lastname'
+          placeholder='Your last name..'
+        />
+
+        <label for='country'>Country</label>
+        <select id='country' name='country'>
+          <option value='australia'>Australia</option>
+          <option value='canada'>Canada</option>
+          <option value='usa'>USA</option>
+        </select>
+
+        <input type='submit' value='Submit' />
+      </form>
+    </>
+  );
+};
+
+export default observer(UserInformationForm);
--- front/src/components/pages/index.js
+++ front/src/components/pages/index.js
@@ -2,5 +2,6 @@ import Main from './Main/Main';
 import Login from './Login/Login';
 import About from './About/About';
 import Contact from './Contact/Contact';
+import UserInformationForm from './UserInformationForm/UserInformationForm';
 
-export { Main, Login, About, Contact };
+export { Main, Login, About, Contact, UserInformationForm };
