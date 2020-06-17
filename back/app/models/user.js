ae91409718178bdb488064296f05f54e21752aad
Robertas <r.ankudovicius@gmail.com>
2020-06-17 18:24:38 +0300

    added some values to user model

--- back/app/models/user.js
+++ back/app/models/user.js
@@ -6,10 +6,12 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
-    match: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+    match: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
   },
   id: { type: String, required: true, min: 5 },
-  picture: { type: String }
+  picture: { type: String },
+  city: { type: String },
+  preferredSports: { type: Array },
 });
 
 module.exports = mongoose.model('User', userSchema);
