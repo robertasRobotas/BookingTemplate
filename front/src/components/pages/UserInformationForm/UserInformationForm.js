d82dbcbfe9638a68c5435db362933fcc6d4d95c4
Robertas <r.ankudovicius@gmail.com>
2020-06-17 17:53:20 +0300

    fixed div bug

--- front/src/components/pages/UserInformationForm/UserInformationForm.js
+++ front/src/components/pages/UserInformationForm/UserInformationForm.js
@@ -6,32 +6,34 @@ import './UserInformationForm.css';
 const UserInformationForm = ({ history, location, rootModel }) => {
   return (
     <>
-      <form>
-        <label for='fname'>First Name</label>
-        <input
-          type='text'
-          id='fname'
-          name='firstname'
-          placeholder='Your name..'
-        />
+      <div className='formContainer'>
+        <form>
+          <label for='fname'>First Name</label>
+          <input
+            type='text'
+            id='fname'
+            name='firstname'
+            placeholder='Your name..'
+          />
 
-        <label for='lname'>Last Name</label>
-        <input
-          type='text'
-          id='lname'
-          name='lastname'
-          placeholder='Your last name..'
-        />
+          <label for='lname'>Last Name</label>
+          <input
+            type='text'
+            id='lname'
+            name='lastname'
+            placeholder='Your last name..'
+          />
 
-        <label for='country'>Country</label>
-        <select id='country' name='country'>
-          <option value='australia'>Australia</option>
-          <option value='canada'>Canada</option>
-          <option value='usa'>USA</option>
-        </select>
+          <label for='country'>Country</label>
+          <select id='country' name='country'>
+            <option value='australia'>Australia</option>
+            <option value='canada'>Canada</option>
+            <option value='usa'>USA</option>
+          </select>
 
-        <input type='submit' value='Submit' />
-      </form>
+          <input type='submit' value='Submit' />
+        </form>
+      </div>
     </>
   );
 };
