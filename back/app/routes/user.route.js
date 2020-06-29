5057468027663389a3b63a96accbe3dfe2e50f42
Robertas <r.ankudovicius@gmail.com>
2020-06-29 15:20:21 +0300

    saved data to db

--- back/app/routes/user.route.js
+++ back/app/routes/user.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const isLoggedIn = require('../middlewares/loginCheck');
+const UserSchema = require('../models/user');
 
 router.get('/getUser', isLoggedIn, (req, res) => {
   return res.status(200).json({ user: req.user });
@@ -16,7 +17,12 @@ router.get('/logout', (req, res) => {
   res.status(200).json({ text: 'loggedOut' });
 });
 
-router.post('/addAdditionalData', isLoggedIn, (req, res) => {
+router.post('/addAdditionalData', isLoggedIn, async (req, res) => {
+  const user = await UserSchema.findOne({ _id: req.user._id });
+  user.preferredSports = req.body.preferredSports;
+  user.city = req.body.city;
+  user.save();
+
   return res.status(200).json({ message: 'ok' });
 });
 
